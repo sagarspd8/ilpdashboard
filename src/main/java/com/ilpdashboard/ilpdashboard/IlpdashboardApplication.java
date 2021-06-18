77f4e225a5cd33b317f6ccdcb10830f124bfc522
Sagar Prasad <sagarspd8@gmail.com>
2021-06-19 02:16:02 +0530

    initial comit

---
+++ src/main/java/com/ilpdashboard/ilpdashboard/IlpdashboardApplication.java
@@ -0,0 +1,13 @@
+package com.ilpdashboard.ilpdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlpdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlpdashboardApplication.class, args);
+	}
+
+}
